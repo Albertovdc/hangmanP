ddb45edfbdfee569f172de353014cfca6ba7f22c
Albertovdc <albertovdc.gnz@gmail.com>
2023-10-22 20:47:34 -0600

    modify main program

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-
 
 print(f'Pssst, the solution is {chosen_word}.')
 
